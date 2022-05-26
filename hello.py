de27d48422bfbc191e94f57889be53c2a1da1ae2
Mills Akyeampong <millsakyeampong@gmail.com>
2022-05-26 03:10:24 -0700

    master

---
+++ hello.py
@@ -0,0 +1,81 @@
+    # 3 diffrent ways of printing text on muttiple lines.
+#print("Hello World")
+#print("My name is Mills")
+
+#print("How you doing dear\nI'm doign good")
+
+#print("""this is the way i want to live
+#since i thingk it is the best way to success""")
+
+    #printing a double and single quote in a string
+#print('here is a double quote"' + "here is a single quote'")
+#print("or you can just do this \" does that work?")
+#print("can I just Print \ on a screen?")
+
+    #inserting a \\ so that \ appears in the string
+#print("But what if I print \\news since \\n is special in pyton")
+
+    #My Poem
+#print("There once was a movie star icon\nwho preferred to sleep with light on.\nThey learned how to code\na device that sure glowed\nand lit up the night using Python!")
+
+    #creating user inputs
+#print("What is your First Name")
+#fname=input()
+#print("What is your Last Name")
+#lname=input()
+#print("Hello " + fname + " " + lname)
+
+#print("What is your favorite animal?")
+#animal=input()
+#print("Name a famous Building")
+#Building=input()
+#print("What is your favorite color?")
+#color=input()
+#print("HIckory Dickory Dock!")
+#print("The " +color+ " ran up the "+Building)
+
+
+#message="Hello World!"
+#print(message.lower())
+#print(message.upper())
+#print(message.swapcase())
+
+
+#print("What Country do you come from?")
+#country=input()
+#country = country.upper()
+#print("Hello, you are from "+country)
+
+
+#message="Hello World"
+#print(message.find("World"))
+#print(message.count("o"))
+#print(message.capitalize())
+#print(message.replace("Hello", "Funny"))
+
+
+#print("Hello, welcome to the postal address unit. Kindly enter your postal code below:")
+#postalcode=input()
+#print(postalcode.upper())
+
+
+
+
+# A PERSONALIZED STORY ABOUT YOURSELF!
+
+
+
+
+
+
+
+
+
+
+
+
+print("")
+print("")
+print("")
+print("")
+print("")
